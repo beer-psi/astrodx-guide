767b46e174c5a300226c4e948f98631e05217ace
beerpiss <lacvtg.a1.2023@gmail.com>
2023-05-21 15:44:09 +0700

    Add Group B testflight

--- scripts/update-testflight-status.py
+++ scripts/update-testflight-status.py
@@ -1,37 +1,47 @@
 import re
+from urllib.parse import quote
 
 import requests
 import bs4
 
 ASTRODX_TESTFLIGHT = "https://testflight.apple.com/join/rACTLjPL"
-STATUS_REGEX = re.compile(r"!\[AstroDX TestFlight status\]\(https://img.shields.io/badge/.+\)")
+ASTRODX_TESTFLIGHTS = {
+    "Group A": "https://testflight.apple.com/join/rACTLjPL",
+    "Group B": "https://testflight.apple.com/join/ocj3yptn",
+}
 STATUS_URLS = {
-    "full": "https://img.shields.io/badge/testflight-full-red",
-    "closed": "https://img.shields.io/badge/testflight-closed-yellow",
-    "open": "https://img.shields.io/badge/testflight-open-green"
+    "full": "https://img.shields.io/badge/{name}-full-red",
+    "closed": "https://img.shields.io/badge/{name}-closed-yellow",
+    "open": "https://img.shields.io/badge/{name}-open-green"
 }
 
 
 def main():
-    page = requests.get(
-        ASTRODX_TESTFLIGHT, 
-        headers={
-            "Accept-Language": "en-us"
-        }
-    ).text
-
-    soup = bs4.BeautifulSoup(page, 'html.parser')
-    status_text = soup.select(".beta-status span")[0].get_text()
-    if "This beta is full." in status_text:
-        status = "full"
-    elif "This beta isn't accepting" in status_text:
-        status = "closed"
-    else:
-        status = "open"
-    
     with open("README.md", newline="\n") as f:
         readme = f.read()
-    readme = STATUS_REGEX.sub(f"![AstroDX TestFlight status]({STATUS_URLS[status]})", readme)
+
+    for name, link in ASTRODX_TESTFLIGHTS.items():
+        page = requests.get(
+            link, 
+            headers={
+                "Accept-Language": "en-us"
+            }
+        ).text
+
+        soup = bs4.BeautifulSoup(page, 'html.parser')
+        status_text = soup.select(".beta-status span")[0].get_text()
+        if "This beta is full." in status_text:
+            status = "full"
+        elif "This beta isn't accepting" in status_text:
+            status = "closed"
+        else:
+            status = "open"
+        
+        status_url = STATUS_URLS[status].format(name=quote(name))
+        
+        regex = re.compile(f"!\\[AstroDX {name} TestFlight status\\]\\(https://img.shields.io/badge/.+?\\)")
+        readme = regex.sub(f"![AstroDX {name} TestFlight status]({status_url})", readme)
+
     with open("README.md", "w", newline="\n") as f:
         f.write(readme)
     
